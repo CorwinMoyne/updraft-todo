7dd4653d039d176ed7cb71e4005c2daf1f751cbe
Corwin Moyne <c_moyne@yahoo.com>
2022-07-22 08:27:03 +0100

    Remove header and test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="App__header">Updraft Todos</h1>
+      {/* <h1 className="App__header">Updraft Todos</h1> */}
       <Todos />
     </div>
   );
