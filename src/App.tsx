a89f69a505451a1b96e28268384ecc8b2c4bd009
Corwin Moyne <c_moyne@yahoo.com>
2022-07-22 12:11:05 +0100

    Add Names

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Todos from "./features/todo/containers/Todos/Todos";
 import "./App.css";
+import Names from "./features/names/Names";
 
 function App() {
   return (
     <div className="App">
       {/* <h1 className="App__header">Updraft Todos</h1> */}
-      <Todos />
+      <Names />
     </div>
   );
 }
---
+++ src/features/names/Names.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import "./Names.css";
+
+interface Props {}
+
+interface Person {
+  id: string;
+  name: string;
+  age: number;
+}
+
+function renderNameAndAge(person: Person) {
+  return `${person.name} is ${person.age} years old`;
+}
+
+const people: Person[] = [
+  { id: "0", name: "Peter", age: 20 },
+  { id: "1", name: "James", age: 25 },
+];
+
+const Names: React.FunctionComponent<Props> = (props) => {
+  return (
+    <div className="names">
+      {people.map((person) => (
+        <div key={person.id} className="name">
+          {renderNameAndAge(person)}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Names;
